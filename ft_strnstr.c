53d5e7975299393ec3b5216dafb60e4506f542ac
Loloo06 <102831930+Loloo06@users.noreply.github.com>
2022-12-12 12:45:59 +0100

    finalpush

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -12,22 +12,22 @@
 
 #include "libft.h"
 
-char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
+char	*ft_strnstr(const char *str1, const char *str2, size_t len)
 {
 	size_t	i;
 	size_t	j;
 
 	i = 0;
-	if (needle[i] == 0)
-		return ((char *)haystack);
-	while (haystack[i] != '\0' && len != 0)
+	if (str2[i] == 0)
+		return ((char *)str1);
+	while (str1[i] != '\0' && len != 0)
 	{
 		j = 0;
-		while (haystack[i + j] == needle[j] && len > (i + j))
+		while (str1[i + j] == str2[j] && len > (i + j))
 		{
 			j++;
-			if (ft_strlen(needle) == j)
-				return ((char *)haystack + i);
+			if (ft_strlen(str2) == j)
+				return ((char *)str1 + i);
 		}
 		i++;
 	}
